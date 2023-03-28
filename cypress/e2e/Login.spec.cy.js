59d3866563d457fdda5cbbb2480d0bb074f772cd
Thiago S. Araujo <Thiago.Araujo@flag.com.br>
2023-03-28 19:34:32 -0300

    Exercício Teste UI

--- cypress/e2e/Login.spec.cy.js
+++ cypress/e2e/Login.spec.cy.js
@@ -1,33 +1,38 @@
 /// <reference types = "cypress" />
 
-context('Funcioalidade Login', () =>{
+context('Funcioalidade Login', () => {
 
-    it('Deve fazer login com sucesso' , () => {
+    beforeEach(() => {
         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+
+    afterEach(() => {
+        cy.screenshot()
+    });
+
+    it('Deve fazer login com sucesso', () => {
         cy.get('#username').type('aluno_ebac@teste.com')
         cy.get('#password').type('teste@teste.com')
         cy.get('.woocommerce-form > .button').click()
 
-    cy.get('.page-title').should('contain' , 'Minha conta')
+        cy.get('.page-title').should('contain', 'Minha conta')
 
-} )
+    })
 
-    it.only(`Deve exibir uma mensagem de erro ao inserir usuário inválido`, () =>{
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    it(`Deve exibir uma mensagem de erro ao inserir usuário inválido`, () => {
         cy.get('#username').type('al.ebac@teste.com')
         cy.get('#password').type('teste@teste.com')
         cy.get('.woocommerce-form > .button').click()
 
-        cy.get('.woocommerce-error > li').should('contain' , 'Endereço de e-mail desconhecido.')
+        cy.get('.woocommerce-error > li').should('contain', 'Endereço de e-mail desconhecido.')
     })
 
-    it('Deve exibir uma mensagem de erro ao inserir a senha inválida' , () => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    it('Deve exibir uma mensagem de erro ao inserir a senha inválida', () => {
         cy.get('#username').type('Aluno_ebac@teste.com')
         cy.get('#password').type('teste123')
         cy.get('.woocommerce-form > .button').click()
 
-        cy.get('.woocommerce-error > li').should('contain' , 'Perdeu a senha?')
+        cy.get('.woocommerce-error > li').should('contain', 'Perdeu a senha?')
     })
 
 })
